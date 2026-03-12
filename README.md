4b52db785841ca1af92f7f0f16ab1d2b32c7495c
Enzo <169556594+enzo963@users.noreply.github.com>
2026-03-13 00:13:03 +0300

    feat(game): add player buttons, update glass styling, and implement score tracking
    - Added rock, paper, scissors buttons for player interaction
    - Improved glass-box styling and hover effects
    - Set up basic human vs computer score system in JS
    - Prepared functions for easier game logic handling

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ```
 # bg --> <https://youtu.be/RGCitnwhD50?si=FXSAtiOWOcTZ4tqk> form this video i Learned how i could add an image as a bg with srvral options     
-#                                                                                                                                              
+# Glass --> https://youtu.be/n77yqF5fdBE?si=ZzrrnX5jbb-hoIG3                                                                                                                                              
 
 
 
--- script.js
+++ script.js
@@ -23,21 +23,96 @@ ELSE
             let number = Math.random();
             getComputerChoice(number)">Random number </button>
     </div>
-
-
 */
 
+/* 
 
+1 - function for computer (actvation auto)
+2 - function for human (actvation by User Input)
+3 - funcation for PlayGame (win 5 and than restart) (this func beasd on our inputs)
+
+*/
 
 function getComputerChoice(number) {
 
     if (number <= 0.3) {
-        return console.log("paper");
+        computerChoice = "paper";
     }
     else if (number >= 0.7) {
-        return console.log("Rock");
+        computerChoice = "rock";
     }
     else {
-        return console.log("Scissore");
+        computerChoice = "scissore";
     }
+
+    return set(0, computerChoice);
 }
+
+
+function HumanChoice(HumanChoice) {
+
+    if (HumanChoice == 1) {
+        HumanChoice = "rock";
+    }
+    else if (HumanChoice == 2) {
+        HumanChoice = "paper";
+    }
+    else if (HumanChoice == 3) {
+        HumanChoice = "scissore";
+    }
+
+    return set(HumanChoice, 0);
+}
+
+
+function set(humanChoice, computerChoice) {
+
+    if (humanChoice == 0)
+        humanChoice = " ";
+    if (computerChoice == 0)
+        computerChoice = " ";
+
+    return playGame(humanChoice, computerChoice);
+}
+
+
+function playGame(HumanChoice, ComputerChoice) {
+
+    if (HumanChoice == "rock" && ComputerChoice == "paper") {
+        console.log(`You :${HumanChoice} Conputer : ${ComputerChoice} | You : Lose`);
+    }
+    else if (HumanChoice == "rock" && ComputerChoice == "scissore") {
+        console.log(`You :${HumanChoice} Conputer : ${ComputerChoice} | You : Win`);
+    }
+    else if (HumanChoice == "paper" && ComputerChoice == "rock") {
+        console.log(`You :${HumanChoice} Conputer : ${ComputerChoice} | You : Win`);
+    }
+    else if (HumanChoice == "paper" && ComputerChoice == "scissore") {
+        console.log(`You :${HumanChoice} Conputer : ${ComputerChoice} | You : lose`);
+    }
+    else if (HumanChoice == "scissore" && ComputerChoice == "paper") {
+        console.log(`You :${HumanChoice} Conputer : ${ComputerChoice} | You : Win`);
+    }
+    else if (HumanChoice == "scissore" && ComputerChoice == "rock") {
+        console.log(`You :${HumanChoice} Conputer : ${ComputerChoice} | You : Win`);
+    }
+    else if (HumanChoice == ComputerChoice) {
+        console.log(`You :${HumanChoice} Conputer : ${ComputerChoice} | Tie`);
+    }
+
+
+
+
+
+
+
+
+/* 
+
+after the user peck one of these Rosk, Paper, Ssissore
+
+maybe we can represent 1, 2, 3 but it is not beatifull in git hub 
+
+
+*/
+
